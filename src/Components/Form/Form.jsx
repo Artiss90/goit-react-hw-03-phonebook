0888bfa02236f1f5497ca3164b6c4254a5c8a530
Artem <64410376+Artiss90@users.noreply.github.com>
2021-02-02 21:51:16 +0200

    добавил пердупреждение

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import style from './Form.module.css';
 import PropTypes from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import style from './Form.module.css';
 
 /* eslint react/prop-types: 1 */
 
@@ -15,10 +17,27 @@ export default class Form extends Component {
   nameInputId = uuidv4();
   numberInputId = uuidv4();
 
+  notify = field =>
+    toast.warn(`поле ${field} не должно бить пустым`, {
+      position: 'top-left',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+
   handleSubmit = e => {
     e.preventDefault();
     const { name, number } = this.state;
-    // console.log(name, number);
+    if (!name) {
+      this.notify('Name');
+    }
+    if (!number) {
+      this.notify('Number');
+    }
+
     if (name && number) {
       this.props.onSubmitForm(this.state);
     }
@@ -60,6 +79,7 @@ export default class Form extends Component {
             id={this.numberInputId}
           />
         </label>
+        <ToastContainer />
         <button type="submit">Add contact</button>
       </form>
     );
